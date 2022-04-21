94ddfc74691908864e8c5905a8b839b08a02f300
Viktoriia <krjuchenkova@gmail.com>
2022-04-21 15:19:44 +0300

    change month after click implementation

--- src/app/shared/services/date.service.ts
+++ src/app/shared/services/date.service.ts
@@ -9,4 +9,9 @@ import * as moment from "moment";
 
 export class DateService {
   public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment());
+
+  changeDate(dir: number) {
+    const value = this.date.value.add(dir, 'month');
+    this.date.next(value);
+  }
 }
